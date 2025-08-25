cb523b2e568c466f8ff7a94fb980076889de6b7d
s2422055 <s2422055@stu.musashino-u.ac.jp>
2025-08-25 15:36:05 +0900

    ログイン機能の追加

--- chatapp/src/components/Login.vue
+++ chatapp/src/components/Login.vue
@@ -20,10 +20,16 @@ const inputUserName = ref("")
 // 入室メッセージをクライアントに送信する
 const onEnter = () => {
   // ユーザー名が入力されているかチェック
+  if (!inputUserName.value.trim()) {
+    alert("ユーザー名を入力してください")
+    return
+  }
 
   // 入室メッセージを送信
-
+  userName.value = inputUserName.value
+  
   // 全体で使用するnameに入力されたユーザー名を格納
+  socket.emit("enterEvent", { name: userName.value })
 
   // チャット画面へ遷移
   router.push({ name: "chat" })
@@ -36,7 +42,7 @@ const onEnter = () => {
     <h1 class="text-h3 font-weight-medium">Vue.js Chat サンプル</h1>
     <div class="mt-10">
       <p>ユーザー名</p>
-      <input type="text" class="user-name-text" />
+      <input type="text" class="user-name-text" v-model="inputUserName" />
     </div>
     <button type="button" @click="onEnter" class="button-normal">入室する</button>
   </div>
