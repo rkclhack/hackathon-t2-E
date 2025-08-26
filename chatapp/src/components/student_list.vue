f24959c86b1268d75cce4bdac9afffea1ddfb51b
Prosamo <s4lm0n.014@gmail.com>
2025-08-26 15:32:47 +0900

    change UIをオレンジに変更

--- chatapp/src/components/student_list.vue
+++ chatapp/src/components/student_list.vue
@@ -7,13 +7,17 @@ const studentName = inject("studentName");
 const activeTab = ref("p1");
 
 const tabs = [
-  { id: "p1", label: "中学一年生", description: "馬場 浩稀" },
-  { id: "p2", label: "中学二年生", description: "佐藤 太郎" },
-  { id: "p3", label: "中学三年生", description: "鈴木 花子" },
-  { id: "p4", label: "高校一年生", description: "高橋 健" },
-  { id: "p5", label: "高校二年生", description: "田中 美咲" },
-  { id: "p6", label: "高校三年生", description: "伊藤 翔" },
+  { id: "p1", label: "中一", description: "馬場 浩稀" , subject: ["国語", "数学"]},
+  { id: "p1", label: "中一", description: "日本 太郎" , subject: ["数学", "理科", "英語"]},
+  { id: "p1", label: "中一", description: "東京 太郎" , subject: ["国語", "数学"]},
+  { id: "p2", label: "中二", description: "佐藤 太郎" , subject: ["国語", "数学"]},
+  { id: "p3", label: "中三", description: "鈴木 花子" , subject: ["国語", "数学"]},
+  { id: "p4", label: "高一", description: "高橋 健" , subject: ["国語", "数学"]},
+  { id: "p5", label: "高二", description: "田中 美咲" , subject: ["国語", "数学"]},
+  { id: "p6", label: "高三", description: "伊藤 翔" , subject: ["国語", "数学"]},
 ];
+
+const tabLabel = ["中一", "中二", "中三", "高一", "高二", "高三"]
 
 const router = useRouter();
 const handleSelectStudent = (tab) => {
@@ -23,6 +27,14 @@ const handleSelectStudent = (tab) => {
 </script>
 
 <template>
+  <div class="head">
+    <span style="font-size: 32px;">生徒一覧</span>
+    <router-link to="/" class="link">
+      <button type="button" class="button-normal button-exit">
+        ログアウト
+      </button>
+    </router-link>
+  </div>
   <div class="container">
     <ul class="tabs">
       <li v-for="tab in tabs" :key="tab.id">
@@ -34,6 +46,16 @@ const handleSelectStudent = (tab) => {
         >
       </li>
     </ul>
+    <ul class="tabs">
+      <li v-for="tab in tabLabel" :key="tab.id">
+        <a
+          href="#"
+          :class="{ active: activeTab === tab.id }"
+          @click.prevent="activeTab = tab.id"
+          >{{ tab }}</a
+        >
+      </li>
+    </ul>
 
     <section
       v-for="tab in tabs"
@@ -42,24 +64,35 @@ const handleSelectStudent = (tab) => {
       class="content"
       v-show="activeTab === tab.id"
     >
-      <span
+      <div
         class="student-name"
         @click="handleSelectStudent(tab)"
         style="cursor: pointer; text-decoration: underline"
       >
         {{ tab.description }}
+      </div>
+      <span v-for="sub in tab.subject">
+        <span class="subject-name">{{sub}}</span>
       </span>
     </section>
-
-    <router-link to="/" class="link">
-      <button type="button" class="button-normal button-exit">
-        ログアウト
-      </button>
-    </router-link>
   </div>
 </template>
 
 <style scoped>
+.head {
+  position: relative
+}
+.button-exit{
+  padding: 2% 3.5%;
+  border-radius:6%;
+  background: #f48356;
+  color: #fff;
+  font-weight: 700;
+  font-size: 95%;
+  position: absolute;
+  right: 0;
+}
+
 .tabs {
   display: flex;
   list-style: none;
@@ -78,9 +111,9 @@ const handleSelectStudent = (tab) => {
 }
 
 .tabs li a.active {
-  background-color: #007bff;
+  background-color: orange;
   color: white;
-  border-color: #007bff;
+  border-color: orange;
 }
 
 .content {
@@ -90,6 +123,13 @@ const handleSelectStudent = (tab) => {
 }
 
 .student-name:hover {
-  color: #007bff;
+  color: orange;
+}
+
+.subject-name {
+  margin-left: 5px;
+  background-color:#ddd;
+  border-radius: 10%;
+  padding: 3px;
 }
 </style>
